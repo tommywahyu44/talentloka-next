5eed099cf7ce69d208711ded8965465e971507ac
Tommy Wahyu <tommywahyu44@gmail.com>
2025-01-23 21:37:26 +0700

    update wa

--- src/components/button/WhatsAppChatButton.jsx
+++ src/components/button/WhatsAppChatButton.jsx
@@ -1,32 +1,54 @@
 'use client'
 
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import ClearIcon from '@mui/icons-material/Clear'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import { Fab } from '@mui/material'
 import clsx from 'clsx'
 import { useState } from 'react'
 
-export default function WhatsAppChatButton() {
-  const currentPath = window.location.pathname
-  const whatsappNumber = '6281299880745' // Replace with your WhatsApp number
-  let message = encodeURIComponent('Hello, I need support!') // Default message
-  const isDashboard = currentPath.includes('dashboard')
-  const [expanded, setExpanded] = useState(false);
-  const handleClickOptions = (optionName) => {
-    if (optionName == 'faq') {
-      window.open(`${window.location.origin}/promotor#faq`, '_blank')
-      return;
-    }
-    if (optionName == 'create_event') {
-      message = encodeURIComponent('How to create event')
-    }
-    if (optionName == 'sign_up') {
-      message = encodeURIComponent('How to sign up')
-    }
-    if (optionName == 'search_spg') {
-      message = encodeURIComponent('How to search spg')
-    }
+const whatsappNumber = '6281299880745' // Replace with your WhatsApp number
+
+const options = [
+  {
+    action: `${window.location.origin}/promotor#faq`,
+    label: 'FAQ',
+  },
+  {
+    action: 'How to create event',
+    label: 'How to create event',
+  },
+  {
+    action: 'How to sign up',
+    label: 'How to sign up',
+  },
+  {
+    action: 'How to search SPG',
+    label: 'How to search SPG',
+  },
+]
+
+function handleClickOptions(option) {
+  let message = ''
+  if (option.label === 'FAQ') {
+    window.location.replace(option.action)
+  } else {
+    message = encodeURIComponent(option.action)
     window.open(`https://wa.me/${whatsappNumber}?text=${message}`, '_blank')
+  }
+}
+
+export default function WhatsAppChatButton() {
+  const [expanded, setExpanded] = useState(false)
+  const currentPath = window.location.pathname
+  const isDashboard = currentPath.includes('dashboard')
+
+  const commonButtonStyles = {
+    backgroundColor: '#f9fafb',
+    width: 'max-content',
+    textTransform: 'none',
+    fontSize: 14, // Assuming you want smaller font size
+    color: 'text.primary', // Assuming you have a theme for color
+    '&:hover': { backgroundColor: '#e8f5e9' },
   }
 
   return (
@@ -35,55 +57,16 @@ export default function WhatsAppChatButton() {
         'fixed right-5 z-50 grid justify-items-end',
         isDashboard ? 'bottom-[5.5rem] sm:bottom-16' : 'bottom-4'
       )}>
-      <div className={clsx('mb-4 grid gap-2 place-items-end', expanded ? null : 'hidden')}>
-        <Fab 
-          onClick={() => handleClickOptions('faq')}
-          sx={{
-            backgroundColor: '#f9fafb',
-            width: 'max-content',
-            textTransform: 'none',
-            '&:hover': { backgroundColor: '#1DA851' },
-          }}
-          variant="extended"
-          color="primary">
-          <span className='text-gray-800'>FAQ</span>
-        </Fab>
-        <Fab
-          onClick={() => handleClickOptions('create_event')}
-          sx={{
-            backgroundColor: '#f9fafb',
-            width: 'max-content',
-            textTransform: 'none',
-            '&:hover': { backgroundColor: '#1DA851' },
-          }}
-          variant="extended"
-          color="primary">
-            <span className='text-gray-800'>How to create event</span>
-        </Fab>
-        <Fab 
-          onClick={() => handleClickOptions('sign_up')}
-          sx={{
-            backgroundColor: '#f9fafb',
-            width: 'max-content',
-            textTransform: 'none',
-            '&:hover': { backgroundColor: '#1DA851' },
-          }}
-          variant="extended"
-          color="primary">
-          <span className='text-gray-800'>How to sign up</span>
-        </Fab>
-        <Fab 
-          onClick={() => handleClickOptions('search_spg')}
-          sx={{
-            backgroundColor: '#f9fafb',
-            width: 'max-content',
-            textTransform: 'none',
-            '&:hover': { backgroundColor: '#1DA851' },
-          }}
-          variant="extended"
-          color="primary">
-          <span className='text-gray-800'>How to search SPG</span>
-        </Fab>
+      <div className={clsx('mb-4 grid place-items-end gap-2', expanded ? null : 'hidden')}>
+        {options.map((option) => (
+          <Fab
+            key={option.label}
+            onClick={() => handleClickOptions(option)}
+            sx={commonButtonStyles}
+            variant="extended">
+            <span>{option.label}</span>
+          </Fab>
+        ))}
       </div>
       <Fab
         color="success"
@@ -93,8 +76,12 @@ export default function WhatsAppChatButton() {
           '&:hover': { backgroundColor: '#1DA851' },
         }}
         className="shadow-lg">
-        <span className={clsx(expanded ? 'hidden' : null)}><WhatsAppIcon sx={{ color: '#ffffff' }}/></span>
-        <span className={clsx(expanded ? null : 'hidden')}><ClearIcon sx={{ color: '#ffffff' }}/></span>
+        <span className={clsx(expanded ? 'hidden' : null)}>
+          <WhatsAppIcon sx={{ color: '#ffffff' }} />
+        </span>
+        <span className={clsx(expanded ? null : 'hidden')}>
+          <ClearIcon sx={{ color: '#ffffff' }} />
+        </span>
       </Fab>
     </div>
   )
